d53e4ebaed368839131c0b6ad916aceb694b611b
Prannay <prannayjain11@gmail.com>
2021-05-16 10:39:40 +0530

    Day 11 completed

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:master_in_flutter/models/catalogue.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key key, @required this.item})
+      : assert(item != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {},
+        leading: Image.asset(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\u{20B9}${item.price}",
+          textScaleFactor: 1.6,
+          style:
+              TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
