b84b14ec19be029446e78363ec49db61ae35a62d
Prannay <prannayjain11@gmail.com>
2021-05-16 11:09:53 +0530

    Day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:master_in_flutter/models/catalogue.dart';
 import 'package:master_in_flutter/widgets/drawer.dart';
+import 'package:master_in_flutter/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
 
   final String name = "Pro-Pj";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(20, (index) => CatalogueModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Catalog App",
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text(context.runtimeType.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -17,7 +17,7 @@ class ItemWidget extends StatelessWidget {
         subtitle: Text(item.desc),
         trailing: Text(
           "\u{20B9}${item.price}",
-          textScaleFactor: 1.6,
+          textScaleFactor: 1.5,
           style:
               TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold),
         ),
